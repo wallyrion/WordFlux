029fc9f60a64a3c4e490b22315257b9cb376e397
Oleksii Korniienko <kornienko1296@gmail.com>
2024-08-05 17:21:44 +0300

    test pipeline

---
+++ WordFlux.ApiService/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WordFlux.ApiService;
+
+// ensure db created
+public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+{
+    public DbSet<WeatherForecast> Forecasts { get; set; }
+}
+
+
+public class WeatherForecast
+{
+    public Guid Id { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public int TemperatureC { get; set; }
+}
