56deb3705cbd1a74fcdf898bc8524d895b6aa75f
Oleksii Korniienko <kornienko1296@gmail.com>
2024-08-17 00:26:59 +0300

    Add custom translation

---
+++ WordFlux.Web/Models/CustomTranslation.cs
@@ -0,0 +1,7 @@
+﻿namespace WordFlux.Web.Models;
+public class CustomTranslation
+{
+    public string? Translation { get; set; }
+    public string? ExampleTranslated { get; set; }
+    public string? ExampleOriginal { get; set; }
+}
