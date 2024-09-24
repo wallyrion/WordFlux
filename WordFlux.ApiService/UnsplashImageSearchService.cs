fa2eb694eb2338bbfa50d366ae0767f325f6d89a
wallyrion <30635131+wallyrion@users.noreply.github.com>
2024-09-25 00:42:32 +0200

    Added unsplash api (#53)

---
+++ WordFlux.ApiService/UnsplashImageSearchService.cs
@@ -0,0 +1,48 @@
+﻿using System.Text.Json.Serialization;
+
+namespace WordFlux.ApiService;
+
+public class UnsplashImageSearchService(IConfiguration configuration)
+{
+    const string baseUrl = "https://api.unsplash.com/";
+    
+    public async Task<List<string>> GetImagesByKeyword(string keyword)
+    {
+        var apiKey = configuration["UnsplashApiKey"];
+        
+        using var client = new HttpClient();
+        
+        client.BaseAddress = new Uri(baseUrl);
+
+        var url = $"/search/photos?client_id={apiKey}&query={keyword}";
+
+        var content = await client.GetAsync(url);
+
+        var response = await content.Content.ReadFromJsonAsync<Response>();
+        
+        //var response = await client.GetFromJsonAsync<Response>(url);
+
+        return response.Results.Select(r => r.Urls.Regular).ToList();
+    }
+    
+   
+}
+
+
+file class Response
+{
+    public List<ResultItem> Results { get; set; }   
+}
+
+file class ResultItem
+{
+    public UrlItem Urls { get; set; }
+}
+
+file class UrlItem
+{
+    [JsonPropertyName("regular")]
+    public string Regular { get; set; }
+}
+
+
