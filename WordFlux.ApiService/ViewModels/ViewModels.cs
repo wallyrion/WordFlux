d1f115d5f14d96429da015499a982e2ce978ccbd
wallyrion <30635131+wallyrion@users.noreply.github.com>
2024-10-01 10:52:31 +0200

    feature/drag-and-drop (#57)
    * Something work with drag and drop
    
    * Drag and drop works finally

---
+++ WordFlux.ApiService/ViewModels/ViewModels.cs
@@ -0,0 +1,3 @@
+﻿namespace WordFlux.ApiService.ViewModels;
+
+public record CardTranslationItemRequest(string Term, string? ExampleTranslated = null, string? ExampleOriginal = null, int Popularity = 0, string? Level = null);
