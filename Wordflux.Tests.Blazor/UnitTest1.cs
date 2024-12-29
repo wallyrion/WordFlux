b17188494f8e8dfc18f08f079b2245edda3a2df9
Oleksii KorniienKo <kornienko1296@gmail.com>
2024-12-29 22:57:20 +0100

    add tests for blazor (do not work)

---
+++ Wordflux.Tests.Blazor/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using BlazeWright;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace Wordflux.Tests.Blazor;
+
+
+internal class CustomWebApplicationFactory : WebApplicationFactory<Program>  
+{
+
+}
+
+/*public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+        using var host = new BlazorApplicationFactory<Program>();
+        
+        
+    }
+}*/
